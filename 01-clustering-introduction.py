1f0a84b692ba13b365ff03308bcc0b377c526fbb
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-07-30 00:48:25 +0530

    Update 01-clustering-introduction.py
    updated

--- 01-clustering-introduction.py
+++ 01-clustering-introduction.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Clustering - Introduction
-# 
-# In contrast to *supervised* machine learning, *unsupervised* learning is used when there is no "ground truth" from which to train and validate label predictions. The most common form of unsupervised learning is *clustering*, which is similar conceptually to *classification*, except that the the training data does not include known values for the class label to be predicted. Clustering works by separating the training cases based on similarities that can be determined from their feature values. Think of it this way; the numeric features of a given entity can be thought of as vector coordinates that define the entity's position in n-dimensional space. What a clustering model seeks to do is to identify groups, or *clusters*, of entities that are close to one another while being separated from other clusters.
-# 
-# For example, let's take a look at a dataset that contains measurements of different species of wheat seed.
-# 
-# > **Citation**: The seeds dataset used in the this exercise was originally published by the Institute of Agrophysics of the Polish Academy of Sciences in Lublin, and can be downloaded from the UCI dataset repository (Dua, D. and Graff, C. (2019). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science).
-
-# In[ ]:
-
-
 import pandas as pd
 
 # load the training dataset
@@ -21,15 +7,6 @@ data = pd.read_csv('seeds.csv')
 # Display a random sample of 10 observations (just the features)
 features = data[data.columns[0:6]]
 features.sample(10)
-
-
-# As you can see, the dataset contains six data points (or *features*) for each instance (*observation*) of a seed. So you could interpret these as coordinates that describe each instance's location in six-dimensional space.
-# 
-# Now, of course six-dimensional space is difficult to visualise in a three-dimensional world, or on a two-dimensional plot; so we'll take advantage of a mathematical technique called *Principal Component Analysis* (PCA) to analyze the relationships between the features and summarize each observation as coordinates for two principal components - in other words, we'll translate the six-dimensional feature values into two-dimensional coordinates.
-
-# In[ ]:
-
-
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
@@ -41,12 +18,6 @@ pca = PCA(n_components=2).fit(scaled_features)
 features_2d = pca.transform(scaled_features)
 features_2d[0:10]
 
-
-# Now that we have the data points translated to two dimensions, we can visualize them in a plot:
-
-# In[ ]:
-
-
 import matplotlib.pyplot as plt
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -56,14 +27,6 @@ plt.xlabel('Dimension 1')
 plt.ylabel('Dimension 2')
 plt.title('Data')
 plt.show()
-
-
-# Hopefully you can see at least two, arguably three, reasonably distinct groups of data points; but here lies one of the fundamental problems with clustering - without known class labels, how do you know how many clusters to separate your data into?
-# 
-# One way we can try to find out is to use a data sample to create a series of clustering models with an incrementing number of clusters, and measure how tightly the data points are grouped within each cluster. A metric often used to measure this tightness is the *within cluster sum of squares* (WCSS), with lower values meaning that the data points are closer. You can then plot the WCSS for each model.
-
-# In[ ]:
-
 
 #importing the libraries
 import numpy as np
@@ -86,11 +49,3 @@ plt.title('WCSS by Clusters')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
-
-
-# The plot shows a large reduction in WCSS (so greater *tightness*) as the number of clusters increases from one to two, and a further noticable reduction from two to three clusters. After that, the reduction is less pronounced, resulting in an "elbow" in the chart at around three clusters. This is a good indication that there are two to three reasonably well separated clusters of data points.
-# 
-# ## Summary
-# 
-# Here we looked at what clustering means, and how to determine whether clustering might be appropriate for your data. In the next notebook, we will look at two ways of 
-# labelling the data automatically.
